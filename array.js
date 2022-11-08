bc7258cc6d586ef37769e136d45b06260a253d6c
Abner Kaizer <abnerkaizer@protonmail.com>
2022-11-08 17:27:40 -0300

    Alterações após o envio, reset depois do submit e refatoração

--- array.js
+++ array.js
@@ -1,23 +1,16 @@
 const form = document.querySelector("form");
-var pessoas = [
-	{ name: "Fabiana Araújo", age: 33},
-	{ name: "Gabriel Gomes", age: 25},
-	{ name: "Fernando Henrique", age: 17},
-	{ name: "Ana Luiza", age: 2},
-	{ name: "Geralda do Nascimento", age: 93},
-	{ name: "Miguel Souza", age: 70},
-	{ name: "Antonio Miguel", age: 69},
-];
+var pessoas = new Array();
 
 function input(e){
 	e.preventDefault();
-	var aux = {name: form.fullname.value, 
+	var aux = {name: form.name.value, 
 	age: form.age.value,
 	job: form.job.value,
 	email: form.email.value,
-	phone: form.phonenumber.value,
+	phone: form.phone.value,
 	date: form.date.value}
 	pessoas.push(aux);
+	form.reset();
 	getPessoas();
 }
 
@@ -45,7 +38,11 @@ function names(){
 
 function insertId(){ 	
 	for (var i = pessoas.length - 1; i >= 0; i--) { 		
-		pessoas[i] = {id: i+1,name: pessoas[i].name, age: pessoas[i].age}; 	
+		pessoas[i] = {id: i+1,name: pessoas[i].name, 
+		age: pessoas[i].age, job: pessoas[i].job, 
+		email: pessoas[i].email, 
+		phone: pessoas[i].phone,
+		date: pessoas[i].date}; 	
 	}
 }
 
